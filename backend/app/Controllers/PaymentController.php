04fc04f2ab63560f234db0c67ec77024f2dc73c7
Flavio Francisco <flavyobass151@gmail.com>
2023-09-26 11:23:44 -0300

    temtando pagamento com react native

--- backend/app/Controllers/PaymentController.php
+++ backend/app/Controllers/PaymentController.php
@@ -5,6 +5,7 @@ namespace App\Controllers;
 use CodeIgniter\API\ResponseTrait;
 use MercadoPago\SDK;
 use MercadoPago\Payment;
+use MercadoPago\Payer;
 
 class PaymentController extends BaseController
 {
@@ -17,21 +18,34 @@ class PaymentController extends BaseController
         SDK::setAccessToken(KEY_MP);
    
 
-        // criação de um pagamento
+        // criação de um <pagamento></pagamento>
         $payment = new Payment();
+        $payer = new Payer();
+            
         $payment->transaction_amount = $data->transaction_amount;
+        $payment->token = $data->token;
         $payment->description = $data->description;
         $payment->payment_method_id = $data->payment_method_id;
-        $payment->payer = [
-            'email' => $data->email,
-            'first_name' => $data->fist_name,
-            'last_name' => $data->last_name,
+        $payment->installments = (int)$data->installments;
+        $payment->issuer_id =$data->issuer_id;
+
+        $payer->email =$data->email;
+        $payer ->identification = [
+           "type" => $data->type,
+           "number"=> $data->fist_name,
+            
         ];
+        $payment->payer =$payer;
 
         //processamento do pagamento
         if ($payment->save()) {
             // Pagamento bem-sucedido
-            return $this->respondCreated('Pagamento realizado com sucesso!', $payment->status);
+            $response = array(
+                'status' => $payment->status,
+                'status_detail' => $payment->status_detail,
+                'id' => $payment->id
+             );
+            return $this->respond($response);
         } else {
             // Falha no pagamento
             return $this->failServerError('Pagamento não realizado!');
